59db047fce1fbdde0136eeb059592e422885fea4
Gwenaël Magnenat <gmg@MacBook-Pro-de-Gwenael-2.local>
2021-02-25 19:33:45 +0100

    Push project to git

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import Page from '../components/Page';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
+}
---
+++ pages/sell.js
@@ -0,0 +1,5 @@
+import Page from '../components/Page';
+
+export default function SellPage() {
+  return <p>hey hey hey</p>;
+}
